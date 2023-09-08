49a3ef0246c50cc1814ac67f37ff181af5fbf1f8
PremPrakash777 <9001premprakash.y@gmail.com>
2023-09-09 00:28:43 +0530

    first

---
+++ demo.js
@@ -0,0 +1,449 @@
+// console.log("hello world");
+// document.write("<h1>this is header</h1>");
+// alert("this is alert");
+
+// var x=20;
+// console.log(x);
+
+// var myname="prem prakash";
+// console.log(myname);
+
+// var username=prompt("what is your name");
+// console.log(username);
+
+// var username=prompt("what is your name");
+// console.log("my name is",username);
+
+// confirm("are you shour to deleat?");
+
+// var x=20;
+// var y=100;
+// var z=146;
+// console.log(x+z+y)
+
+// console.log(x-z-y)
+
+
+// console.log(x*z*y)
+
+// console.log(x/z/y)
+
+// console.log(x%z%y)
+
+
+// pi = 3.141;
+// function findArea(r) {
+// console.log(pi * r * r);
+// }
+// var r, Area;
+// r = 6;
+// Area = findArea(r);
+// console.log("Area of Circle is: " + Area);
+
+
+
+
+// var area = a * b;
+// var a=4;
+//  var b=5;
+// console.log(2*a*b)
+// console.log("Area of rectangle")
+
+// var a=5;
+// var b=5;
+
+// console.log(b)
+
+// let a=5;
+// let b=5;
+
+// console.log(b)
+
+// const a=5 
+// const a=59
+// console.log(a)
+
+
+// this is import**********
+// var a= "ajay"+500-300+"ajay";
+// console.log(a);
+
+
+// var a= "ajay"+500-300+"ajay"+300;
+// console.log(a);
+
+// let a= "ajay"/500+300+"ajau"+500+"ajay";
+// console.log(a)
+
+// const a=10+10+10+"ajay";
+// console.log(a)
+
+// function sum(a,b,b)
+//  console.log(a+b+b)
+
+
+// var a=20;
+// b=30;
+
+// if(a==b){
+//     console.log("a is this")
+// }
+// else{
+//     console.log("b is this")
+// }
+
+// if(a===b){
+//     console.log("a is this")
+// }
+// else{
+//     console.log("b is this")
+// }
+
+// if(a<=b){
+//     console.log("a is this")
+// }
+// else{
+//     console.log("b is this")
+// }
+
+// if(a>=b){
+//     console.log("a is this")
+// }
+// else{
+//     console.log("b is this")
+// }
+
+// var a=10;
+// var b=20;
+// var c=30;
+
+// if(a>b && b>c)
+// {
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+
+// if(a>b || b>c)
+// {
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+// console.log(!10==10)
+
+// var a=10;
+// b=20;
+// c=30;
+// d=40;
+
+// if(a>b){
+//     console.log("a is grater")
+// }
+// else if(b>c){
+//     console.log("a is grater")
+// }
+// else if(c>d){
+//     console.log("a is grater")
+// }
+// else if(d>a){
+//     console.log("a is grater")
+// }
+// else{
+//     console.log("not")
+// }
+
+// var student=10;
+// student=20
+ 
+
+// if(student=85>=100){
+//     console.log("1st division")
+// }
+// else if(student=75>=84){
+//     console.log("2st division")
+// }
+// else if(student=60>=74){
+//     console.log("3st division")
+// }
+// else if(student=45>=59){
+//     console.log("4st division")
+// }
+// else if (student=0>=35){
+//     console.log("fail")
+// }
+// else{
+//     console.log("not a marks include")
+// }
+ 
+
+
+// write to input any alphabate and chack whether it is vowel or conconante?  
+// var ch='a';
+// if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+//     console.log("is a vowel")
+// }
+// else if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+//     console.log("is a vowel")
+// }
+// else{
+//     console.log("is a consonant")
+
+// }
+
+// write a to wheather a character is alphabate or Not
+// var ch=/^[a-zA-Z]+$/;
+// a=A;
+// if (a=ch)
+// {
+//     console.log("is a consonant")
+// }
+
+// else{
+//     alert("please enter the letter only")
+// }
+
+
+// var day=5;
+
+// switch(day){
+//     case 1:
+//     {
+//         console.log("today is monday")
+//     }
+//     break;
+//     case 1:
+//     {
+//         console.log("today is tuesday")
+//     }
+//     break;
+//     case 1:
+//     {
+//         console.log("today is wednesday")
+//     }
+//     break;
+//     case 1:
+//     {
+//         console.log("today is thursday")
+//     }
+//     break;
+//     case 1:
+//     {
+//         console.log("today is friday")
+//     }
+//     break;
+//     case 1:
+//     {
+//         console.log("today is sutrday")
+//     }
+//     break;
+//     case 7:
+//     {
+//         console.log("today is sunday")
+//     }
+//     break;
+//     default:
+//         {
+//             console.log("data is wrong")
+//         }
+    
+
+// }
+
+
+// var year=11;
+
+// switch(year){
+//     case 1:{
+//         console.log("it jan")
+//     }
+//     break;
+//     case 2:{
+//         console.log("it fev")
+//     }
+//     break;
+//     case 3:{
+//         console.log("it mar")
+//     }
+//     break;
+//     case 4:{
+//         console.log("it apr")
+//     }
+//     break;
+//     case 5:{
+//         console.log("it may")
+//     }
+//     break;
+//     case 6:{
+//         console.log("it jun")
+//     }
+//     break;
+//     case 7:{
+//         console.log("it july")
+//     }
+//     break;
+//     case 8:{
+//         console.log("it agust")
+//     }
+//     break;
+//     case 9:{
+//         console.log("it sep")
+//     }
+//     break;
+//     case 10:{
+//         console.log("it oct")
+//     }
+//     break;
+//     case 11:{
+//         console.log("it nov")
+//     }
+//     break;
+//     case 12:{
+//         console.log("it dec")
+//     }
+//     break;
+//     default:
+//     {
+//          console.log("data is wrong")
+//     }
+// }
+
+    // object
+
+// var x={}
+// console.log(x);
+
+// let obj1={
+//     uname:"ajay",
+//     lname:"prem",
+//     age:23,
+//     mode:"jaipur",
+// };
+// console.log(obj1);
+// console.log(obj1.mode)
+
+// let obj1={
+//     uname:"ajay",
+//     position:"HR",
+//     Eid:"ajay singh",
+//     city:"jaipur",
+//     salary:"40000"
+// };
+// console.log(obj1);
+// console.log(obj1.salary)
+
+// var user={
+//     fname:"aman",
+//     lname:"sharma",
+//     age:25,
+//     gender:"mail",
+//     fruits:["apple","banaba","grapes","orange"]
+
+// }
+// console.log(user);
+// console.log(user.fruits[3]);
+
+// var obj1={
+//     uname:"ajay",
+//     lname:"loio",
+//     salary:function()
+//     {
+//         return 25000
+//     }
+// }
+// console.log(obj1);
+// console.log(obj1.salary());
+
+
+// var movie={
+//     fristfav:"f&f8",
+//     secfev:"avatara",
+//     detail:{
+//         city:"jaipur",
+//         state:"raj"
+//     }
+// }
+// console.log(movie);
+// console.log(movie.detail.state);
+
+
+
+// var mom={
+// marks:function (eng,hindi,maths){
+//     console.log(hindi+eng+maths)
+// }  
+// }
+// console.log(mom);
+// console.log(mom.marks(98,50,60));
+
+
+// var a=new Object()
+// {
+//     a.fristname="ajay",
+//     a.lastname="kumar",
+//     a.age=20,
+//     a.email="ajay@gmail.com"
+// }
+// console.log(a);
+// console.log(a.lastname)
+
+// arrow function
+
+// var hello=function()
+// {
+//     console.log("hello world")
+// } 
+// hello();
+
+
+
+// hello=()=>{
+//     console.log("hello world")
+// }
+// hello();
+
+// info=(uname,age)=>
+// {
+//     return "hello my name is ajay"+"my age is "+age;
+
+// }
+// console.log(info("ajay",35));
+
+
+
+// console.log((1+2)*100/2);
+// console.log(1+2=="3");
+// console.log(1+2=== "3");
+// console.log(1+"2"+"2");
+// console.log(1+ -"1" +"2");
+// console.log("1"-"2"+"2");
+// console.log("A"-"B"+"2");
+// console.log("A"-"B"+2);
+
+// var x = 21;
+// var girl = function () {
+//     console.log(x)
+// }
+// girl ()
+
+// const x = 21;
+// var girl = function () {
+//     console.log(x)
+//     x=30;
+// }
+// girl ()
+
+
+// const x = 21;
+// var girl = function () {
+//     x=30;
+//     console.log(x)
+// }
+// girl ()
+
+
+
